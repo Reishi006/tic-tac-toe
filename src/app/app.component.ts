be05abaf9f87314d6859c5f32ae6e9e2cd98c5db
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-06-30 07:19:45 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,100 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+class Box {
+  constructor(index: number, value: string, imagePath: string) {
+    this.index = index;
+    this.value = value;
+    this.imagePath = imagePath
+  }
+
+  index = 0;
+  value = '';
+  imagePath = '';
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tic-tac-toe';
+  boxes: Box[] = [];
+
+  player: string[] = ['x', 'o'];
+  currentTurn = Math.floor(Math.random() * 2);
+  result: string = '';
+  gameEnd: boolean = true;
+
+
+  ngOnInit() {
+    for (let i = 0; i < 9; i++) {
+      this.boxes.push(new Box(i, '', ''));
+    }
+
+    console.log(this.boxes);
+    console.log(this.currentTurn);
+  }
+
+  setBoxImage(i: number) {
+    if (this.currentTurn < 0 || this.currentTurn > 1) return;
+    if (this.boxes[i].value !== '') return;
+
+    if (this.currentTurn === 0) {
+      this.boxes[i].value = this.player[this.currentTurn];
+      this.boxes[i].imagePath = `${this.player[this.currentTurn]}.svg`;
+      this.currentTurn = 1;
+    } else if (this.currentTurn === 1) {
+      this.boxes[i].value = this.player[this.currentTurn];
+      this.boxes[i].imagePath = `${this.player[this.currentTurn]}.svg`;
+      this.currentTurn = 0;
+    }
+  }
+
+  checkWin() {
+    const conditions: number[][] = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 4, 8],
+      [2, 4, 6],
+
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    for (const condition of conditions) {
+      const box1 = this.boxes[condition[0]];
+      const box2 = this.boxes[condition[1]];
+      const box3 = this.boxes[condition[2]];
+
+      if (box1.value !== '' && box1.value === box2.value && box1.value === box3.value) {
+        this.result = `Player ${box1.value} wins!`;
+        this.gameEnd = true;
+        return;
+      }
+    }
+  }
+
+  handleBoxClick(i: number) {
+    console.log(i);
+
+    this.setBoxImage(i);
+    this.checkWin();
+  }
+
+  resetBoard() {
+    this.boxes = [];
+    for (let i = 0; i < 9; i++) {
+      this.boxes.push(new Box(i, '', ''));
+    }
+
+    this.result = '';
+    this.gameEnd = false;
+  }
 }
