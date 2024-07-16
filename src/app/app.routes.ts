7b050d08d152c09dca16cd27f89f43f5d9336816
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-07-16 06:43:19 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { KeyboardControlComponent } from './keyboard-control/keyboard-control.component';
 
 
-export const routes: Routes = [{path: '', component: AppComponent}]; //TO DO: add keyboard controls component
+export const routes: Routes = [{path: '', component: AppComponent}, {path: 'keyboard-control', component: KeyboardControlComponent}];
