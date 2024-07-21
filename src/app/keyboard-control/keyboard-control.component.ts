6560cae9e5c7215fe5a7341b84e7d2cab0c22374
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-07-21 20:17:17 +0200

    Update keyboard-control.component.ts

--- src/app/keyboard-control/keyboard-control.component.ts
+++ src/app/keyboard-control/keyboard-control.component.ts
@@ -1,13 +1,28 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { captureError } from 'rxjs/internal/util/errorContext';
 
 
 @Component({
   selector: 'app-keyboard-control',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './keyboard-control.component.html',
   styleUrl: './keyboard-control.component.scss'
 })
 export class KeyboardControlComponent {
+  indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  letters = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'];
 
+  capitalLetters: string[] = [];
+
+  ngOnInit() {
+    this.capitalLetters = this.letters.map(el => el.toUpperCase());
+
+    this.logMe();
+  }
+
+  logMe() {
+    console.log(this.letters);
+  }
 }
